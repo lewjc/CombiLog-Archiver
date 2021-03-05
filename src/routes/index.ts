1976f0b23b635e3338ec41d0a543ccba3264a3d9
lewjc <lewisjcummins@hotmail.co.uk>
2021-03-05 16:09:27 +0000

    Add version endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import * as express from "express";
-
-// import sub-routers
 import file from "./file/index";
+import { version } from "../../package.json";
 
 const router: express.Router = express.Router();
 
 router.use("/file", file);
 
+router.get("/version", async (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    version,
+  });
+});
 // Export the router
 export default router;
